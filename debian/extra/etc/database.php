c816b3f89de2d8d3a220ad97661eea879cfdd8e1
Deepin Developer <deepin-dev@deepin.org>
2022-10-08 15:57:17 +0800

    feat: Init commit
    init hoteldruid
    
    Log:

---
+++ debian/extra/etc/database.php
@@ -0,0 +1,27 @@
+<?php
+# When you insert a value for one of the following variables, it will
+# always be used by hoteldruid to connect to the database
+
+# Database type, values can be "sqlite", "mysql" or "postgresql"
+$HOTELD_DB_TYPE = "";
+
+# Database name
+$HOTELD_DB_NAME = "";
+
+# Database connection host
+$HOTELD_DB_HOST = "";
+
+# Database connection port
+$HOTELD_DB_PORT = "";
+
+# Username to authenticate with database
+$HOTELD_DB_USER = "";
+
+# Password to authenticate with database
+$HOTELD_DB_PASS = "";
+
+#  Prefix used in front of all table names
+$HOTELD_TAB_PRE = "";
+
+
+?>
